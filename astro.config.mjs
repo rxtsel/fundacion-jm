3ca74b358566d4a887828ab91c85c5fb98afc12a
Cristhian Melo <rxtsel@hotmail.com>
2022-08-24 18:38:26 -0500

    fix: remove error loading css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://rxtsel.github.io',
-  base: ''
+  base: '/',
+  trailingSlash: 'always'
 })
