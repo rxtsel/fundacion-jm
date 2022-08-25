bc64a3f8d677cacb58dcc41ffa4515e9e43d1aa7
Cristhian Melo <rxtsel@hotmail.com>
2022-08-24 19:07:50 -0500

    chore: change metatag icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://rxtsel.github.io',
-  base: '/',
-  trailingSlash: 'always'
+  base: '/'
 })
