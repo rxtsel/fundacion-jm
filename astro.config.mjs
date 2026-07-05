028807cb90474403ef915c4127d54e06c472dc68
Cristhian Melo <rxtsel@outlook.com>
2026-07-05 16:00:29 -0500

    build: add custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  site: "https://fundacionjm.com",
 });
+
